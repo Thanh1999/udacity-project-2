8cc48fbeb7975ca3787efe52baaabd2a3410b3ea
Thanh1999 <t.max347056@gmail.com>
2023-05-28 19:55:05 +0700

    clean cmd

--- src/util/util.ts
+++ src/util/util.ts
@@ -12,7 +12,6 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
       const photo = await Jimp.read(inputURL);
-      console.log('created photo');
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
 
